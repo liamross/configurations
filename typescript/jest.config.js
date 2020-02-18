fde2a46e1589e02b6ad9b0c97843148f3dbc4e5a
Liam Ross <24969218+liamross@users.noreply.github.com>
2020-02-18 07:19:18 -0800

    Update jest.config.js

--- typescript/jest.config.js
+++ typescript/jest.config.js
@@ -1,16 +1,7 @@
 module.exports = {
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  collectCoverage: true,
-  coverageDirectory: './coverage/',
-
-  testMatch: [
-    // Match any typescript file ending with ComponentName.test.(ts|tsx).
-    '**/?(*.)test.(ts|tsx)',
-  ],
-
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-  },
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/?(*.)+(spec|test).[jt]s?(x)'],
 
   // Setup Enzyme (don't include if no enzyme)
   snapshotSerializers: ['enzyme-to-json/serializer'],
